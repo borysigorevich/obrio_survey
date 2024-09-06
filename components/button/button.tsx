6291eee869e4784c8169740a844f53e65baa1cbc
borysigorevich <borysigorevich@gmail.com>
2024-09-06 10:03:13 +0300

    update button paddings

--- components/button/button.tsx
+++ components/button/button.tsx
@@ -9,7 +9,7 @@ export const Button = ({ className, children, ...props }: ButtonProps) => {
 		<button
 			{...props}
 			className={cn(
-				`px-5 py-[21px] rounded-2xl shadow-primary border bg-button border-button text-sm leading-[22.4px]
+				`px-5 py-3 flex items-center justify-center h-16 rounded-2xl shadow-primary border bg-button border-button text-sm leading-[22.4px]
 				 		text-button-default active:text-button-active active:bg-button-active`,
 				className
 			)}
